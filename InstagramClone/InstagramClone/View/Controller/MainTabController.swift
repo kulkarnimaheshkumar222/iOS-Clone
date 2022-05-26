07afa9d2baba84af96dc44ffb1a87df01f3de7ce
Mahesh Kulkarni <maheshkulkarni@Maheshs-MacBook-Pro.local>
2022-05-27 00:05:01 +0530

    configure project set up

---
+++ InstagramClone/InstagramClone/View/Controller/MainTabController.swift
@@ -0,0 +1,16 @@
+//
+//  MainTabController.swift
+//  InstagramClone
+//
+//  Created by Mahesh Kulkarni on 26/05/22.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+}
---
+++ InstagramClone/InstagramClone/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by Mahesh Kulkarni on 26/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        view.backgroundColor = .blue
+    }
+
+
+}
+
